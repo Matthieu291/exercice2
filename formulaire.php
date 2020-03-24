a58fff1d72e3fb41a71a9ede2773cb929582331d
Matthieu291 <matt.diard@gmail.com>
2020-03-24 16:31:00 +0100

    ex2

--- formulaire.php
+++ formulaire.php
@@ -1,3 +1,30 @@
 <?php
-echo "Exercice 2"
+echo "<h1>Exercice 2</h1>";
+
+class formulaire {
+
+    public function __construct(){
+        echo "
+<form action ='/testformulaire.php'> "
+;
+    }
+
+    public function ajouterzonetexte($string = NULL){
+        echo "<b>$string :";
+        echo" <input type='text'>";
+        echo"<br>";
+    }
+
+    public function ajouterbouton(){
+        echo "<input type='submit'> ";
+    }
+
+    public function getform() {
+
+        $this->ajouterzonetexte();
+        $this->ajouterbouton();
+        echo "</form>";
+    }
+}
+
 ?>
---
+++ testformulaire.php
@@ -0,0 +1,8 @@
+<?php
+include 'formulaire.php';
+
+$f1 = new formulaire();
+$f1->ajouterzonetexte("Votre nom");
+$f1->ajouterzonetexte("Votre code");
+$f1->ajouterbouton();
+?>
